09232a144c2fbdb79bd382ea3808f06b9cd8a43b
Amar Prakash Pandey <amar.om1994@gmail.com>
2015-09-29 19:13:25 +0530

    check

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); /* Starts the session */
-
-if(!isset($_SESSION['UserData']['Username'])){
-	header("location:login.php");
-	exit;
-}
-?>
-
-Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.
---
+++ script.js
@@ -0,0 +1,231 @@
+
+
+$(document).ready(function(){
+	$('.tig1').click(function(){
+		$('#fade1').fadeToggle();
+		$('#fade2').fadeToggle();
+		$('#fade3').fadeToggle();
+		$('#fade4').fadeToggle();
+		$('#fade5').fadeToggle();
+		$('#fade6').fadeToggle();
+		$('#fade7').fadeToggle();
+		$('.eventssub').toggleClass('eclass');
+		$('.fade').toggleClass('fclass');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig2').click(function(){
+		$('#fade11').fadeToggle();
+		$('#fade12').fadeToggle();
+		$('#fade13').fadeToggle();
+		$('#fade14').fadeToggle();
+		$('#fade15').fadeToggle();
+		$('#fade16').fadeToggle();
+		$('#fade17').fadeToggle();
+		$('.eventssub2').toggleClass('eclass2');
+		$('.fade2').toggleClass('fclass2');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig3').click(function(){
+		$('#fade21').fadeToggle();
+		$('#fade22').fadeToggle();
+		$('#fade23').fadeToggle();
+		$('#fade24').fadeToggle();
+		$('#fade25').fadeToggle();
+		$('#fade26').fadeToggle();
+		$('#fade27').fadeToggle();
+		$('.eventssub3').toggleClass('eclass3');
+		$('.fade3').toggleClass('fclass3');
+	});
+});
+
+$(document).ready(function(){
+	$('.tig4').click(function(){
+		$('#fade31').fadeToggle();
+		$('#fade32').fadeToggle();
+		$('#fade33').fadeToggle();
+		$('#fade34').fadeToggle();
+		$('#fade35').fadeToggle();
+		$('#fade36').fadeToggle();
+		$('#fade37').fadeToggle();
+		$('.eventssub4').toggleClass('eclass4');
+		$('.fade4').toggleClass('fclass4');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig5').click(function(){
+		$('#fade41').fadeToggle();
+		$('#fade42').fadeToggle();
+		$('#fade43').fadeToggle();
+		$('#fade44').fadeToggle();
+		$('#fade45').fadeToggle();
+		$('#fade46').fadeToggle();
+		$('#fade47').fadeToggle();
+		$('.eventssub5').toggleClass('eclass5');
+		$('.fade5').toggleClass('fclass5');
+	});
+});
+
+
+
+$(document).ready(function(){
+	$('.tig6').click(function(){
+		$('#fade51').fadeToggle();
+		$('#fade52').fadeToggle();
+		$('#fade53').fadeToggle();
+		$('#fade54').fadeToggle();
+		$('#fade55').fadeToggle();
+		$('#fade56').fadeToggle();
+		$('#fade57').fadeToggle();
+		$('.eventssub6').toggleClass('eclass6');
+		$('.fade6').toggleClass('fclass6');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig7').click(function(){
+		$('#fade61').fadeToggle();
+		$('#fade62').fadeToggle();
+		$('#fade63').fadeToggle();
+		$('#fade64').fadeToggle();
+		$('#fade65').fadeToggle();
+		$('#fade66').fadeToggle();
+		$('#fade67').fadeToggle();
+		$('.eventssub7').toggleClass('eclass7');
+		$('.fade7').toggleClass('fclass7');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig8').click(function(){
+		$('#fade71').fadeToggle();
+		$('#fade72').fadeToggle();
+		$('#fade73').fadeToggle();
+		$('#fade74').fadeToggle();
+		$('#fade75').fadeToggle();
+		$('#fade76').fadeToggle();
+		$('#fade77').fadeToggle();
+		$('.eventssub8').toggleClass('eclass8');
+		$('.fade8').toggleClass('fclass8');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig9').click(function(){
+		$('#fade81').fadeToggle();
+		$('#fade82').fadeToggle();
+		$('#fade83').fadeToggle();
+		$('#fade84').fadeToggle();
+		$('#fade85').fadeToggle();
+		$('#fade86').fadeToggle();
+		$('#fade87').fadeToggle();
+		$('.eventssub9').toggleClass('eclass9');
+		$('.fade9').toggleClass('fclass9');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig10').click(function(){
+		$('#fade91').fadeToggle();
+		$('#fade92').fadeToggle();
+		$('#fade93').fadeToggle();
+		$('#fade94').fadeToggle();
+		$('#fade95').fadeToggle();
+		$('#fade96').fadeToggle();
+		$('#fade97').fadeToggle();
+		$('.eventssub10').toggleClass('eclass10');
+		$('.fade10').toggleClass('fclass10');
+	});
+});
+
+
+
+// row going on 10
+
+
+
+
+$(document).ready(function(){
+	$('.tig11').click(function(){
+		$('#fade101').fadeToggle();
+		$('#fade102').fadeToggle();
+		$('#fade103').fadeToggle();
+		$('#fade104').fadeToggle();
+		$('#fade105').fadeToggle();
+		$('#fade106').fadeToggle();
+		$('#fade107').fadeToggle();
+		$('.eventssub11').toggleClass('eclass11');
+		$('.fade11').toggleClass('fclass11');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig12').click(function(){
+		$('#fade111').fadeToggle();
+		$('#fade112').fadeToggle();
+		$('#fade113').fadeToggle();
+		$('#fade114').fadeToggle();
+		$('#fade115').fadeToggle();
+		$('#fade116').fadeToggle();
+		$('#fade117').fadeToggle();
+		$('.eventssub12').toggleClass('eclass12');
+		$('.fade12').toggleClass('fclass12');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig13').click(function(){
+		$('#fade121').fadeToggle();
+		$('#fade122').fadeToggle();
+		$('#fade123').fadeToggle();
+		$('#fade124').fadeToggle();
+		$('#fade125').fadeToggle();
+		$('#fade126').fadeToggle();
+		$('#fade127').fadeToggle();
+		$('.eventssub13').toggleClass('eclass13');
+		$('.fade13').toggleClass('fclass13');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig14').click(function(){
+		$('#fade131').fadeToggle();
+		$('#fade132').fadeToggle();
+		$('#fade133').fadeToggle();
+		$('#fade134').fadeToggle();
+		$('#fade135').fadeToggle();
+		$('#fade136').fadeToggle();
+		$('#fade137').fadeToggle();
+		$('.eventssub14').toggleClass('eclass14');
+		$('.fade14').toggleClass('fclass14');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.tig15').click(function(){
+		$('#fade141').fadeToggle();
+		$('#fade142').fadeToggle();
+		$('#fade143').fadeToggle();
+		$('#fade144').fadeToggle();
+		$('#fade145').fadeToggle();
+		$('#fade146').fadeToggle();
+		$('#fade147').fadeToggle();
+		$('.eventssub15').toggleClass('eclass15');
+		$('.fade15').toggleClass('fclass15');
+	});
+});
