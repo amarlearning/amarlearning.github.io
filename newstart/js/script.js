517dd5c621c1c071aa03505169fc0d0d0b652d86
Amar Pandey <amar.om1994@gmail.com>
2016-01-24 02:19:35 +0530

    script and css updated

--- newstart/js/script.js
+++ newstart/js/script.js
@@ -6,23 +6,23 @@ var opts = {
   loop: true,
   postfix: ''
 };
-malarkey(elem, opts).type('Amar.').pause(700).delete()
-.type('a nerd.').pause(700).delete()
-.type('sexy.').pause(700).delete()
-.type('Quora addict.').pause(700).delete()
-.type('Introvert.').pause(700).delete()
-.type('Sapiosexual.').pause(700).delete()
-.type('a Geek.').pause(700).delete()
-.type('a Web Developer.').pause(700).delete()
-.type('a Hacker.').pause(700).delete()
-.type('that guy.').pause(700).delete()
+malarkey(elem, opts).type('Amar.').pause(700).delete(100)
+.type('a nerd.').pause(700).delete(100)
+.type('sexy.').pause(700).delete(100)
+.type('Quora addict.').pause(700).delete(100)
+.type('Introvert.').pause(700).delete(100)
+.type('Sapiosexual.').pause(700).delete(100)
+.type('a Geek.').pause(700).delete(100)
+.type('a Web Developer.').pause(700).delete(100)
+.type('a Hacker.').pause(700).delete(100)
+.type('that guy.').pause(700).delete(100)
 .type('Amar.').pause(700).type(' And I love building things with code.').pause(1200000000000);
 
 
 $(document).ready(function(){
   setTimeout(function(){
     $('#hide').css({'display':'none'});
-  },22100);
+  },21000);
 });
 $(document).ready(function(){
   $('#github').hover(function(){
