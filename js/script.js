62a3957210b3a05783ced5906794dd2c211e46a8
Amar Prakash Pandey <amar.om1994@gmail.com>
2020-09-15 23:29:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,11 +7,9 @@ var opts = {
 	postfix: ''
 };
 malarkey(elem, opts).type('Amar.').pause(1000).delete(100)
-.type('a nerd.').pause(700).delete(100)
 .type('Quora addict.').pause(700).delete(100)
 .type('Introvert.').pause(700).delete(100)
-.type('a Geek.').pause(700).delete(100)
-.type('a Web Developer.').pause(700).delete(100)
+.type('a Java guy.').pause(700).delete(100)
 .type('a Hacker.').pause(700).delete(100)
 .type('GSoCer.').pause(700).delete(100)
 .type('that guy.').pause(700).delete(100)
@@ -19,12 +17,11 @@ malarkey(elem, opts).type('Amar.').pause(1000).delete(100)
 
 var online = new Object();
 online.github = "I dump code on Github.";
+online.stackoverflow = "Giving back to the community."
 online.dev  = "Sometimes I write ;)";
 online.twitter  = "Mostly retweet on Twitter.";
 online.linkedin  = "Be professional on LinkedIn.";
 online.quora  = "Ask questions on Quora.";
-online.facebook  = "Make friends on Facebook.";
-online.googleplus  = "Circle me here.";
 
 $(document).ready(function(){
 	
