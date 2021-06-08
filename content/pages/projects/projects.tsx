bc66613f4443054b45bc6b26af28f3ddd3e29cd7
Amar Prakash Pandey <amar.om1994@gmail.com>
2021-06-08 16:01:25 +0000

    Add template for projects section

---
+++ content/pages/projects/projects.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Projects() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </header>
+
+            <div
+                className="row row-cols-auto"
+                style={{ justifyContent: 'space-around' }}
+            >
+                <div className="col" style={{ marginTop: 20 }}>
+                    <div className="card" style={{ width: 300 }}>
+                        <img
+                            src={`https://images.unsplash.com/photo-1623138997819-2b17f0a1f06d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNzYxMTB8MHwxfGFsbHw2fHx8fHx8Mnx8MTYyMzE2Mzc1Nw&ixlib=rb-1.2.1&q=80&w=400`}
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col" style={{ marginTop: 20 }}>
+                    <div className="card" style={{ width: 300 }}>
+                        <img
+                            src={`https://images.unsplash.com/photo-1623138997819-2b17f0a1f06d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNzYxMTB8MHwxfGFsbHw2fHx8fHx8Mnx8MTYyMzE2Mzc1Nw&ixlib=rb-1.2.1&q=80&w=400`}
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col" style={{ marginTop: 20 }}>
+                    <div className="card" style={{ width: 300 }}>
+                        <img
+                            src={`https://images.unsplash.com/photo-1623138997819-2b17f0a1f06d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNzYxMTB8MHwxfGFsbHw2fHx8fHx8Mnx8MTYyMzE2Mzc1Nw&ixlib=rb-1.2.1&q=80&w=400`}
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col" style={{ marginTop: 20 }}>
+                    <div className="card" style={{ width: 300 }}>
+                        <img
+                            src={`https://images.unsplash.com/photo-1623138997819-2b17f0a1f06d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNzYxMTB8MHwxfGFsbHw2fHx8fHx8Mnx8MTYyMzE2Mzc1Nw&ixlib=rb-1.2.1&q=80&w=400`}
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col" style={{ marginTop: 20 }}>
+                    <div className="card" style={{ width: 300 }}>
+                        <img
+                            src={`https://images.unsplash.com/photo-1623138997819-2b17f0a1f06d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNzYxMTB8MHwxfGFsbHw2fHx8fHx8Mnx8MTYyMzE2Mzc1Nw&ixlib=rb-1.2.1&q=80&w=400`}
+                            className="card-img-top"
+                            alt="..."
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
