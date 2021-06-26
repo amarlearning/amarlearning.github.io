b4a36a554a6233319d85cc9cf3b002ab718e6fd9
Amar Prakash Pandey <amar.om1994@gmail.com>
2021-06-26 19:56:21 +0530

    Remove GSoc year from About section

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         siteImage: `/meta-image-default.png`,
         siteTitle: `Amar Prakash Pandey`,
         siteTitleAlt: `Amar Prakash Pandey`,
-        siteDescription: `This is the personal website of Amar Prakash Pandey. Solutions Consultant at Sahaj. Past TRDDC, TCS, GSoC 2017. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
+        siteDescription: `This is the personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
         siteLanguage: `en`,
     },
     plugins: [
@@ -55,7 +55,7 @@ module.exports = {
             options: {
                 name: `Amar Prakash Pandey`,
                 short_name: `Amar`,
-                description: `This is the personal website of Amar Prakash Pandey. Solutions Consultant at Sahaj. Past TRDDC, TCS, GSoC 2017. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
+                description: `This is the personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
                 start_url: `/`,
                 background_color: `#FFFFFF`,
                 theme_color: `#3AAFA9`,
