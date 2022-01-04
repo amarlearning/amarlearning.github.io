512e253240b9346ff8caa81ec8e7acaa54e8e13b
Amar Prakash Pandey <amar.om1994@gmail.com>
2022-01-04 23:31:51 +0530

    Feat: Add banner images for blogs Fixed metadata of website

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,16 @@ require(`dotenv`).config({
 });
 
 module.exports = {
-    pathPrefix: `/test`,
+    pathPrefix: `/`,
     siteMetadata: {
         author: `Amar Prakash Pandey`,
-        siteUrl: `https://www.amarpandey.me/test/`,
-        siteHeadline: `This is the personal website of Amar Prakash Pandey.`,
+        siteUrl: `https://www.amarpandey.me/`,
+        siteHeadline: `Personal website of Amar Prakash Pandey.`,
         titleTemplate: `%s — Amar Prakash Pandey`,
         siteImage: `/meta-image-default.png`,
         siteTitle: `Amar Prakash Pandey`,
         siteTitleAlt: `Amar Prakash Pandey`,
-        siteDescription: `This is the personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
+        siteDescription: `Personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
         siteLanguage: `en`,
     },
     plugins: [
@@ -55,7 +55,7 @@ module.exports = {
             options: {
                 name: `Amar Prakash Pandey`,
                 short_name: `Amar`,
-                description: `This is the personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
+                description: `Personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
                 start_url: `/`,
                 background_color: `#FFFFFF`,
                 theme_color: `#3AAFA9`,
