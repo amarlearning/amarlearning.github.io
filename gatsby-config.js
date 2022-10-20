d937bdb5974f0736f571bc82cecda689a3feb3d4
Amar Prakash Pandey <amar.om1994@gmail.com>
2022-10-20 20:47:16 +0530

    fix deployment pipeline to github pages
    Change favicon file type from ico to png

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
             options: {
                 name: `Amar Prakash Pandey`,
                 short_name: `Amar`,
-                description: `Personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. I have also worked for Tata Research Design and Development Center, Tata Consultancy Service, and Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
+                description: `Personal website of Amar Prakash Pandey. Currently working as a Solutions Consultant at Sahaj Software. Past Google Summer of Code. Open Source Community Member at DuckDuckGo. Maintainer at Open Source Help Community.`,
                 start_url: `/`,
                 background_color: `#FFFFFF`,
                 theme_color: `#3AAFA9`,
